3667c9a69164d28215beb87333dbe2a2984a2fe3
Egor Malykh <hello@meownoid.pro>
2020-10-06 19:23:34 +0300

    set default docs html theme

--- docs/conf.py
+++ docs/conf.py
@@ -11,6 +11,6 @@ extensions = [
 ]
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-html_theme = 'alabaster'
+html_theme = 'default'
 html_static_path = ['_static']
 master_doc = 'index'
